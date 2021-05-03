fc77ae45f5d287c524ad0a5146cc519799e8db5c
NayeliYunuen <201821564@tese.edu.mx>
2021-05-03 18:25:39 -0500

    Villanos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ContadorComponent } from './contador/contador.component';
+import { VillanosComponent } from './villanos/villanos.component';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeroeComponent,
+      ContadorComponent,
+      VillanosComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-villano.ts
@@ -0,0 +1,8 @@
+import {Villano} from './villano';
+export const VILLANOS: Villano[] = [
+    {id: 12, name: ' VENOM', nivel: 7500, poder: 'Adherirse a cualquier cuerpo y levantar 15 toneladas'},
+    {id: 13, name: ' FÉNIX OSCURA', nivel: 10000, poder: 'Habilidad manitular materia y energia'},
+    {id: 14, name: ' ANNIHILUS', nivel: 9999999, poder: 'Manipular energia cosmica y la estructura molecular de la materia'},
+    {id: 15, name: ' LOKI', nivel: 59999, poder: 'Proyecciones astrales'},
+    {id: 16, name: ' KANG', nivel: 2099, poder: 'Genio extraordinario'},
+];
